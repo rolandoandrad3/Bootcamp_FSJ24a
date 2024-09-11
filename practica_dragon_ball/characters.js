fea33e754c11081ef88bb8a6f707a47f3cd5034d
KeniaKodigo <kenia.paz@kodigo.org>
2024-09-10 20:28:55 -0600

    lista de personajes, planetas dragon ball

--- practica_dragon_ball/characters.js
+++ practica_dragon_ball/characters.js
@@ -6,10 +6,85 @@ export async function getAllCharacters() {
         const response = await fetch("https://dragonball-api.com/api/characters");
         const data = await response.json();
         console.log(data);
-        
+
+        //data.items
+        //accedemos a la propidad items (donde se encuentran los personajes)
+        const { items } = data;
+
+        //LLAMANDO AL CONTENEDOR PARA LA LISTA DE PERSONAJES
+        const dragonBallContainer = document.getElementById('list-dragon-ball');
+
+        //return items;
+        //for of => iterar elementos de arreglos y objetos
+        for(const character of items){
+            //accdemos al id y nombre de cada personaje
+            const { id, name } = character
+            //console.log(`Personaje: ${name}, ID: ${id}`);
+            
+            //https://dragonball-api.com/api/characters/1
+            const response_character = await fetch(`https://dragonball-api.com/api/characters/${id}`);
+
+            const data_character = await response_character.json()
+            //console.log(data_character);
+            //desestructurando el objeto de cada personaje
+            const { name: nameCharacter, race, image, originPlanet, transformations } = data_character
+
+            //desestructurando el objeto del planeta
+            const { name: planet } = originPlanet 
+
+            const itemTransformation = transformations.map(({ name }) => name)
+            //console.log("Transformaciones: ", itemTransformation); //[]
+            
+            //creando un nuevo elemento html (article)
+            const article = document.createElement('article');
+            //operador ternario ? proceso : otra cosa
+            article.innerHTML = `
+                <h4>${nameCharacter}</h4>
+                <img src=${image} >
+                <p>Raza: ${race}</p>
+                <p>Planeta: ${planet}</p>
+                <p>Tranformaciones: ${itemTransformation.length > 0 ? itemTransformation.join(', ') : 'No hay transformaciones'}</p>
+            `;
+            //agregar un hijo al contenedor de la lista de personajes
+            dragonBallContainer.appendChild(article)
+        }
     }catch(error){
         console.log('Error al obtener la data de dragon ball', error);
     }
 }
+
+//Metodo para el consumo de planetas
+export async function getPlanets() {
+    try{
+        const response = await fetch("https://dragonball-api.com/api/planets?limit=20")
+        const data = await response.json();
+        console.log(data);
+
+        //desestructurando el objeto "data"
+        const { items } = data;
+
+        const containerPlanet = document.querySelector('#planet');
+        //creando un select (etiqueta)
+        const select = document.createElement('select');
+
+        for(const planet_data of items){
+            //console.log(planet_data);
+
+            //desestructurar la info del planeta
+            const { name } = planet_data;
+            console.log(name);
+            select.innerHTML += `
+                <option value="${name}">${name}</option>
+            `;
+            //appendChild
+            containerPlanet.appendChild(select);
+        }
+    }catch(error){
+        console.log("Error al obtener los planetas", error);
+    }
+}
+
+
+
 
 
--- practica_dragon_ball/main.js
+++ practica_dragon_ball/main.js
@@ -3,8 +3,9 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import { poke, type, id, abilities, item1, item2, item3, saludar } from './desestructuracion.js'
-import { getAllCharacters } from './characters.js'
+import { getAllCharacters, getPlanets } from './characters.js'
 
+getPlanets();
 getAllCharacters();
 
 saludar()
@@ -21,20 +22,11 @@ console.log(`${poke} tiene estas habilidades: ${item1}, ${item2}, ${item3}`);
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1>API DRAGON BALL</h1>
+    <div id="planet">
+        <label>Seleccion un planeta: </label>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div id="list-dragon-ball"></div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
