030d5b9e13a248eea52e61a88c522d21d4fb5e94
KeniaKodigo <kenia.paz@kodigo.org>
2024-10-14 21:06:55 -0600

    listar / registrar productos

--- practica_firebase_productos/src/components/ListProducts.jsx
+++ practica_firebase_productos/src/components/ListProducts.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import db from '../firebase/appConfig'
 
 export default function ListProducts() {
+    //declaramos un estado para la lista de productos
+    const [products, setProducts] = useState([])
+
+    //montando la informacion de los productos que hay en firebase
+    useEffect(() => {
+        //Funcion que nos permite visualizar la info de la bd en tiempo real
+        onSnapshot(
+            //obtenemos la conexion de la base de datos y el nombre de la coleccion
+            collection(db, "products"),
+            (snapshot) => {
+                //objeto de firebase
+                //console.log(snapshot);
+                //testeando el primer documento de la coleccion
+                console.log(snapshot.docs[0].data());
+                
+                /** mapeando / iterando los documentos de la coleccion */
+                const array_products = snapshot.docs.map((doc) => {
+                    //copiamos la data de cada documento de la coleccion productos y la mandamos al array_products
+                    return {...doc.data(), id: doc.id}
+                })
+                //testear 
+                console.log(array_products);
+                
+                //actualizamos el estado con el arreglo de productos
+                setProducts(array_products)
+            }
+        )
+    }, [])
+
     return (
-        <div>ListProducts</div>
+        <div>
+            <h2>Lista de Productos</h2>
+            <div>
+                {
+                    products.length > 0 ?
+                        products.map((product) => {
+                            return (
+                                <div key={product.id}>
+                                    <div>
+                                        <h3>{product.name}</h3>
+                                        <p>{product.description}</p>
+                                        <button>Editar</button>
+                                        <button>Eliminar</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    : <p>No hay productos por el momento</p>
+                }
+            </div>
+        </div>
     )
 }
--- practica_firebase_productos/src/components/RegisterProduct.jsx
+++ practica_firebase_productos/src/components/RegisterProduct.jsx
@@ -1,7 +1,49 @@
+import { addDoc, collection } from 'firebase/firestore';
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import db from '../firebase/appConfig';
 
 export default function RegisterProduct() {
+    const { register, handleSubmit, watch, formState: {errors} } = useForm()
+    /**
+     * register = hace referencia a lo que capturo en la entrada de dato
+     * watch = permite observar alguna entrada de dato (valor)
+     * handleSubmit = es la accion de lo que voy hacer con la informacion
+     */
+
+    console.log(watch('name'));
+    //metodo para guardar un producto
+    const saveProduct = async () => {
+        console.log("Se ha guardado");
+        
+        //conectarnos a la bd y guardamos un documento
+        try{
+            await addDoc(collection(db, "products"), {
+                name: "Test",
+                description: "Test"
+            })
+        }catch(error){
+            console.error("Error al registrar el producto", error)
+        }
+    }
+    
     return (
-        <div>RegisterProduct</div>
+        <div>
+            <h2>Registro de Productos</h2>
+            <form action="" onSubmit={handleSubmit(saveProduct)}>
+                <div>
+                    <label htmlFor="">Ingresar Producto</label>
+                    <input type="text" {...register('name')}/>
+                </div>
+
+                <div>
+                    <label htmlFor="">Descripcion</label>
+                    <input type="text" {...register('description')}/>
+                </div>
+                <div>
+                    <button type='submit'>Guardar Producto</button>
+                </div>
+            </form>
+        </div>
     )
 }
